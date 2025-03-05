03dae639241135195b2a813c03eac1b460a199c3
curdin derungs <curdin.derungs@hslu.ch>
2025-03-05 13:05:32 +0000

    works with delta boston

--- src/main_rf.py
+++ src/main_rf.py
@@ -28,7 +28,6 @@ def main():
     parser.add_argument("--data", type=str, help="name of registered dataset")
     parser.add_argument("--test_train_ratio", type=float, required=False, default=0.25)
     parser.add_argument("--n_estimators", required=False, default=100, type=int)
-    parser.add_argument("--learning_rate", required=False, default=0.1, type=float)
     parser.add_argument("--registered_model_name", type=str, help="model name")
     args = parser.parse_args()
    
@@ -46,27 +45,26 @@ def main():
     print("input data:", args.data)
 
     # load registered data
-
     subscription_id = '08539d0e-620d-424c-aea8-56853ed8fe3e'
     resource_group = 'dspro2'
     workspace_name = 'dspro2ml'
 
     workspace = Workspace(subscription_id, resource_group, workspace_name)
 
-    dataset = Dataset.get_by_name(workspace, name='bostonhousing')
+    dataset = Dataset.get_by_name(workspace, name=args.data)
     pddf_bh = dataset.to_pandas_dataframe()
     
     mlflow.log_metric("num_samples", pddf_bh.shape[0])
     mlflow.log_metric("num_features", pddf_bh.shape[1] - 1)
 
     # Your training code goes here
-    target = "medv"
+    target = "target"
 
     X = pddf_bh.drop(target, axis=1)
     y = pddf_bh[target]
 
     # Split the data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_train_ratio, random_state=42)
 
     ###################
     #</prepare the data>
@@ -76,8 +74,7 @@ def main():
     #<train the model>
     ##################
 
-    n_estimators = 100
-    clf = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
+    clf = RandomForestRegressor(n_estimators=args.n_estimators, random_state=42)
     clf.fit(X_train, y_train)
 
     # Evaluate the model
